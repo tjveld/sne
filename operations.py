e464804a8ab01ed5b5d358f1d88d9462546029eb
Tim in 't Veld <44731985+tjveld@users.noreply.github.com>
2022-05-31 21:50:11 +0200

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,23 @@
+# COMMON OPERATIONS
+x + y      #Add
+x - y      #Subtract
+x * y      #Multiply
+x / y      #Divide (produces a float)
+x // y     #Floor Divide (produces an integer)
+x % y      #Modulo (remainder)
+x ** y     #Power
+x << n     #Bit shift left
+x >> n     #Bit shift right
+x & y      #Bit-wise AND
+x | y      #Bit-wise OR
+x ^ y      #Bit-wise XOR
+~x         #Bit-wise NOT
+abs(x)     #Absolute value
+
+# LOGIC OPERATORS
+x < y      #Less than
+x <= y     #Less than or equal
+x > y      #Greater than
+x >= y     #Greater than or equal
+x == y     #Equal to
+x != y     #Not equal to
